f35fefdac7b3c11b570e131a0074ba2a63fd3a25
talhaumercop <talhaumar097@example.com>
2025-03-14 17:25:13 +0300

    Initial commit

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,6 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
-import ArrowDown from '@/assets/icons/arrow-down.svg';
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcons from "@/assets/icons/star.svg";
 import SparkleIcons from "@/assets/icons/sparkle.svg";
@@ -16,34 +16,34 @@ export const HeroSection = () => {
       <div className="size-[1020px] hero-ring"></div>
       <div className="size-[1220px] hero-ring"></div>
       <HeroOrbit Orbit OrbitDuration={30} size={350} rotation={98}>
-        <StarIcons className='size-8 text-emerald-300' />
+        <StarIcons className="size-8 text-emerald-300" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={32} size={430} rotation={-18}>
-        <SparkleIcons className='size-5 text-emerald-300/20' />
+        <SparkleIcons className="size-5 text-emerald-300/20" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={34} size={440} rotation={80}>
-        <SparkleIcons className='size-2 text-emerald-300/40' />
+        <SparkleIcons className="size-2 text-emerald-300/40" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={36} size={520} rotation={-40}>
-        <div className='size-2 rounded-full text-emerald-300' />
+        <div className="size-2 rounded-full text-emerald-300" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={38} size={540} rotation={180}>
-        <SparkleIcons className='size-10 text-emerald-300/20' />
+        <SparkleIcons className="size-10 text-emerald-300/20" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={40} size={550} rotation={0}>
-        <StarIcons className='size-12 text-emerald-300' />
+        <StarIcons className="size-12 text-emerald-300" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={42} size={650} rotation={-5}>
-        <div className='size-5 rounded-full text-emerald-300' />
+        <div className="size-5 rounded-full text-emerald-300" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={44} size={710} rotation={144}>
-        <SparkleIcons className='size-14 text-emerald-300/20' />
+        <SparkleIcons className="size-14 text-emerald-300/20" />
       </HeroOrbit>
       <HeroOrbit shouldSpin spinDuration={2}   Orbit OrbitDuration={46} size={720} rotation={85}>
-        <div className='size-5 rounded-full text-emerald-300' />
+        <div className="size-5 rounded-full text-emerald-300" />
       </HeroOrbit>
       <HeroOrbit Orbit OrbitDuration={58} size={800} rotation={-45}>
-        <StarIcons className='size-28 text-emerald-300' />
+        <StarIcons className="size-28 text-emerald-300" />
       </HeroOrbit>
      
     </div>
@@ -66,12 +66,12 @@ export const HeroSection = () => {
           <span className="font-semibold">
             Explore My Work
           </span>
-          <ArrowDown className='size-4' />
+          <ArrowDown className="size-4" />
         </button>
         <button className="inline-flex items-center gap-2 border px-6 h-12 rounded-xl border-white bg-white text-gray-900">
           <span>✋</span>
           <span className="font-semibold">
-            Let's Connect
+            Let&apos;s Connect
           </span>
         </button>
       </div>
